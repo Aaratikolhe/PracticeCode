5b1cc9f42d4017236c7a36de3da3e697313df548
Aaratikolhe <kolhe.aarati@gmail.com>
2022-05-26 10:11:02 +0530

    Adding new gradebook project having only Program class with array and list declaration

---
+++ README.md
@@ -0,0 +1,1 @@
+#Parctice codes during traing
---
+++ gradebook/src/Gradebook/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book=new Book();
+            var numbers=new []{12.7,10.3,6.11,4.1};
+            List<double> grades=new List<double>(){12.7,10.3,6.11,4.1};
+            grades.Add (56.1);
+            // numbers[0]=12.7;
+            // numbers[1]=10.2;
+            // numbers[2]=10.3;
+
+            var result = 0.0;
+            // result=result+numbers[1];
+            // result+=numbers[2];
+            foreach (var number in grades)
+            {
+                result+=number;
+            }
+            result /=grades.Count;
+            Console.WriteLine($"The average grade is {result:N5}");
+            if(args.Length>0)
+            {
+                Console.WriteLine($"Hello, {args[0]}" );
+            }
+            
+            else
+            {
+
+             Console.WriteLine("Hello, Aarati!");
+
+            }
+        }
+    }
+}
+
