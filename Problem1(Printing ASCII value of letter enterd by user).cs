2ca2408747e914c3d7ad46228d7b02c2fbf86805
Aaratikolhe <kolhe.aarati@gmail.com>
2022-06-02 18:22:25 +0530

    Added Problem1 project solved problem statement for Checking user input as smallletter,Uppercase or spcial character

---
+++ Problem1(Printing ASCII value of letter enterd by user).cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class problem1
+{
+    public static vod Main(string[] args)
+    {
+        char letter;
+        Console.WriteLine("Enter Letter");
+        letter = Console.Read();
+        Console.WriteLine("letter");
+    }
+
+
+}
---
+++ Problem1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+public class problem1
+{
+    public static void Main(string[] args) 
+    {
+        TakeChar();
+
+        void TakeChar() 
+        {
+            int letter;
+
+            Console.WriteLine("Enter Letter");
+            //letter = Convert.ToChar(Console.ReadLine());
+            Int32.TryParse(Console.ReadLine(), out letter);
+
+            if (letter >= 65 && letter <= 90)
+            {
+                Console.WriteLine($"The Character is Capital letter ");
+            }
+            if (letter >= 65 && letter <= 90)
+            {
+                Console.WriteLine($"The Character is Small letter ");
+            }
+            if (letter >= 48 && letter <= 57)
+            {
+                Console.WriteLine($"The Character you entered is a number ");
+            }
+            //if(letter == 0 && letter <= 47 || letter >= 58 && letter <= 64 || letter >= 91 && letter <= 96 || letter >= 123 && letter <= 127)
+            else
+            {
+                Console.WriteLine("The Character you entered is a special character ");
+            }
+            char choice;
+            Console.WriteLine("Enter c to continue and q to exit");
+            choice = Convert.ToChar(Console.ReadLine());
+            switch (choice)
+            {
+                case 'c':
+                    Console.WriteLine("Your choice is to continue with code");
+                    TakeChar();
+                    break;
+
+                case 'q':
+                    break;
+
+                default:
+                    break;
+
+
+            }
+
+        }
+    
+    }
+
+}
+
