05defd40b6027d007c798021561f79b7a593995e
Aaratikolhe <kolhe.aarati@gmail.com>
2022-06-03 17:00:59 +0530

    Problem2 & problem3 project added for solutions of sample problem statement solved

--- Problem2/Program.cs
+++ Problem2/Program.cs
@@ -1,2 +1,92 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ * In this example, two classes, Cube and Square, implement an abstract class, Shape, and override its abstract Area property. 
+ * The program accepts the side as an input and calculates the areas for the square and cube. 
+ * It also accepts the area as an input and calculates the corresponding side for the square and cube.
+ * */
+abstract class Shape
+{
+    private double area;
+    public abstract double Area
+    {
+        get;
+        set;
+    }
+}
+
+    class Square : Shape
+    {
+
+    private double side;
+    public double Side { get; set; }
+
+    public override double Area
+        {
+            get
+            {
+                return side * side;
+            }
+            set
+            {
+                Side = Math.Sqrt(value);
+            }
+        }
+
+    public Square(double side)
+    {
+        this.side = side;
+    }
+}
+class Cube : Shape
+{
+    private double side;
+    public double Side { get => side; set => side = value; }
+
+    public override double Area 
+    {
+        get
+        {
+            return Side * Side * Side;
+        }
+        set 
+        {
+            Side = Math.Cbrt(value); 
+        }
+       }
+
+    
+    public Cube(double side)
+    {
+        this.Side = side;
+    }
+}
+
+class TestShapes
+{
+    public static void Main(String[] args)
+    { 
+
+        double side;
+        Console.WriteLine("Enter side");
+        //double side = Convert.ToDouble(Console.ReadLine());
+        Double.TryParse(Console.ReadLine(),out side);
+        Square sq = new Square(side);
+        Cube cube=new Cube(side);
+
+        
+        Console.WriteLine($"Area of square={sq.Area:N4}");
+        Console.WriteLine($"Area of cube={cube.Area:N4}");
+
+        double area;
+        Console.WriteLine("Enter area");
+        //double side = Convert.ToDouble(Console.ReadLine());
+        Double.TryParse(Console.ReadLine(), out area); 
+
+        sq.Area = area;
+        cube.Area = side;
+        Console.WriteLine($"side of square of area {area}={sq.Side:N4}");
+        Console.WriteLine($"Side of cube of area {area}={cube.Side:N4}");
+
+    }
+   
+}
+
---
+++ Problem3/Program.cs
@@ -0,0 +1,60 @@
+﻿/*Write a program using a switch statement that takes one value from the user and asks about the type of conversion and 
+ * then performs a conversion depending on the type of conversion.*/
+class Problem3
+{
+    public static void Main(String[] args)
+    {
+        //double value;
+        double cm, litre, km,kg,inch;
+        int choice;
+        Console.WriteLine("****Digit Convertor****");
+        Console.WriteLine("1. Convert from  inches to centimetres \n2. Convert from Centimetres to inches \n3. Convert from centimetres to kilometres \n4. Convert from kilometres to centimetres  \n5. Convert from gallons to litres \n6. Convert from pounds to kilograms \n7. Quit");
+        
+        do
+        {
+            Console.WriteLine("Enter your choice for conversion");
+            
+            Int32.TryParse(Console.ReadLine(), out choice);
+            Console.WriteLine("Enter a number for conversion");
+            double number;
+            Double.TryParse(Console.ReadLine(), out number);
+
+            switch (choice)
+            {
+                case 1:
+                    cm = number / 2.54;
+                    Console.WriteLine($"The value from inches to Centimetres = {cm}");
+                    break;
+                case 2:
+                    inch = number * 2.54;
+                    Console.WriteLine($"The value from centimetres to Inches = {inch}");
+                    break;
+                case 3:
+                    km = number / 100000;
+                    Console.WriteLine($"The value in kilometres = {km}");
+                    break;
+                case 4:
+                    cm = number * 100000;
+                    Console.WriteLine($"The value from kilometres to Centimetres = {cm}");
+                    break;
+                case 5:
+                    litre = number * 3.785;
+                    Console.WriteLine($"The value from gallons to litres = {litre}");
+                    break;
+                case 6:
+                    kg = number * 2.20;
+                    Console.WriteLine($"The value from pounds to kilogram = {kg}");
+                    break;
+                case 7:
+                    break;
+                default:
+                    Console.WriteLine("No choice for conversion...Ending the program");
+                    break;
+            }
+           
+        }
+        while (choice != 7);
+    }
+
+    }
+
