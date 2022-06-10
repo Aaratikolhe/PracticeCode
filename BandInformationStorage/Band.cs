1898f82513c132f941eba61246327a21025ecd3e
Aaratikolhe <kolhe.aarati@gmail.com>
2022-06-10 17:43:20 +0530

    BandInformationStorage and MethodObjectAndLoops_BandDataStorage project added for code performed during C# Method,object and loops course module

---
+++ BandInformationStorage/Band.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BandInformationStorage
+{
+   class Band
+    {
+
+        public string Name;
+        int Musician;
+        public void Announce()
+        {
+            Console.WriteLine("Welcome " + Name + " to the stage");
+        }
+    }
+}
---
+++ BandInformationStorage/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace BandInformationStorage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("What is the name of your band");
+            //string name = Console.ReadLine();
+            //AnnounceBand(name)
+
+
+            Band band = new Band();
+            band.Name = Console.ReadLine();
+            band.Announce();
+        }
+        //public static void AnnounceBand(string bandName)
+        //{
+        //    Console.WriteLine("Welcome " + bandName + "to the stage");
+        //}
+    }
+
+}
